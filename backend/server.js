7e45634b32aaac2c9897aec81ab38abb3f9534df
Anand <anandsingh14442@gmail.com>
2025-10-02 19:47:31 +0530

    upadte the server

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,14 @@ connectDB();
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true,
+}));
 app.use(express.json());
 
 // Routes
+app.get("/", (req, res) => res.send("API is running..."));
 app.use('/api/auth', authRoutes);
 app.use('/api/favorites', favoriteRoutes);
 
