bef7649b4f3207aa95268d3c3889841d547a45ea
Anand <anandsingh14442@gmail.com>
2025-10-02 20:07:39 +0530

    update the server for vercel

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173' , 'https://yum-vibes-r8e6.vercel.app'],
   credentials: true,
 }));
 app.use(express.json());
